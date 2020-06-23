fcafbae7f7e3cc5b597ecb939052ac20d998576a
AJ Twiss <ajtwiss@AJs-MacBook-Pro.local>
2020-06-23 07:12:03 -0700

    created a more dynamic approach to finding sound related to button and click

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const handleClickBinded = (e) => {
   console.log(allButtons);
   for (let i = 0; i < allButtons.length; i++) {
     allButtons[i].addEventListener("click", (e) => {
-      console.log(` clicked ${e.target.classList[0]}`);
-      var audio = new Audio("./sounds/tom-1.mp3");
+      console.log(` clicked ${e.target.classList[2]}`);
+      var pathToAudio = e.target.classList[2];
+      var audio = new Audio(`./sounds/${pathToAudio}.mp3`);
       audio.play();
     });
   }
