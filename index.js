4e99a1ffd5e17279b814ad43f31248517ef5624c
AJ Twiss <ajtwiss@AJs-MacBook-Pro.local>
2020-06-23 07:35:01 -0700

    changed to use es6 syntax

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const handleClickBinded = (e) => {
   let allButtons = document.querySelectorAll(".drum");
-  console.log(allButtons);
   for (let i = 0; i < allButtons.length; i++) {
     allButtons[i].addEventListener("click", (e) => {
       console.log(` clicked ${e.target.classList[2]}`);
-      var pathToAudio = e.target.classList[2];
-      var audio = new Audio(`./sounds/${pathToAudio}.mp3`);
+      let pathToAudio = e.target.classList[2];
+      let audio = new Audio(`./sounds/${pathToAudio}.mp3`);
       audio.play();
     });
   }
 };
 
-window.onload = function () {
+window.onload = () => {
   handleClickBinded();
 };
