583dc155d23ab17cb810323d3e3cc206b4fe0adb
AJ Twiss <ajtwiss@AJs-MacBook-Pro.local>
2020-06-24 05:42:17 -0700

    adding keydown event listner

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const handleClickBinded = (e) => {
   let allButtons = document.querySelectorAll(".drum");
   for (let i = 0; i < allButtons.length; i++) {
+    document.addEventListener("keydown", (e) => {
+      console.log(` keydown ${e.code}`);
+    });
     allButtons[i].addEventListener("click", (e) => {
       console.log(` clicked ${e.target.classList[2]}`);
       let pathToAudio = e.target.classList[2];
